e04bccf2b299a13b06048eb16740d9d5ac0d7f56
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-07-24 13:31:54 +0200

    PyGame implementation of views removed

--- rest_api/mobile_application/views.py
+++ rest_api/mobile_application/views.py
@@ -1,49 +1,7 @@
 from flask import Blueprint, jsonify
 
-import pygame
-from pygame.locals import *
-import sys, os
-import time
-
 mobile_application = Blueprint('mobile_application', __name__)
-
 
 @mobile_application.route('/')
 def index():
     return jsonify({"greeting": "Hello, World!"})
-
-
-pygame.init()
-pygame.display.set_caption('Identi-Kreate')
-mouse = pygame.mouse
-fpsClock = pygame.time.Clock()
-width = 1200
-height = 600
-
-window = pygame.display.set_mode((width, height))
-#canvas = window.copy()
-
-#                     R    G    B
-BLACK = pygame.Color( 0 ,  0 ,  0 )
-WHITE = pygame.Color(255, 255, 255)
-BROWN = pygame.Color(161, 117, 73)
-
-
-#canvas.fill(WHITE)
-
-while True:
-  left_pressed, middle_pressed, right_pressed = mouse.get_pressed()
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
-    #elif left_pressed:
-    #  pygame.draw.circle(canvas, BLACK, (pygame.mouse.get_pos()),5)
-  window.fill(WHITE)
-#   pygame.draw.polygon(window, BLACK, ((25,75),(76,125),(250,375),(400,25),(60,540)))
-  #pygame.draw.circle(window, BROWN, (600, 300),75)
-  rectangle = Rect(400, 100, 300, 400)
-  pygame.draw.ellipse(window, BROWN, rectangle)
-  #window.blit(canvas, (0, 0))
-  #pygame.draw.circle(window, BLACK, (pygame.mouse.get_pos()), 5)
-  pygame.display.update()
