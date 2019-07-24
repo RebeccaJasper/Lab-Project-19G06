53ee21e4208d3c6991ad734bc357d18ca00fe8ae
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-07-24 11:54:55 +0200

    Landmarks placed on static image

--- rest_api/mobile_application/controller.py
+++ rest_api/mobile_application/controller.py
@@ -1,40 +1,31 @@
-# Instructions to run in anaconda terminal:
-# activate activate opencv-env
-# navigate to folder with file in it
-# python controller.py
-# conda deactivate to close opencv env
-
 import cv2
 import numpy as np
 import dlib
 
-cap = cv2.VideoCapture(0)
-
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+predictor = dlib.shape_predictor("landmarks_data/shape_predictor_68_face_landmarks.dat")
 
-while True:
-    _, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+img = cv2.imread('facial_images/1.jpg', 0)
 
-    faces = detector(gray)
-    for face in faces:
-        x1 = face.left()
-        y1 = face.top()
-        x2 = face.right()
-        y2 = face.bottom()
-        #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+faces = detector(img)
 
-        landmarks = predictor(gray, face)
+for face in faces:
+    x1 = face.left()
+    y1 = face.top()
+    x2 = face.right()
+    y2 = face.bottom()
+    #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
 
-        for n in range(0, 68):
-            x = landmarks.part(n).x
-            y = landmarks.part(n).y
-            cv2.circle(frame, (x, y), 4, (255, 0, 0), -1)
+    landmarks = predictor(img, face)
+
+    for n in range(0, 68):
+        x = landmarks.part(n).x
+        y = landmarks.part(n).y
+        cv2.circle(img, (x, y), 4, (255, 0, 0), -1)
 
 
-    cv2.imshow("Frame", frame)
+cv2.imshow('Output', img)
 
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
