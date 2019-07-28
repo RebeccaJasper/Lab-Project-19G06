5c4d44e08ae3d473c9ebe61835476f0962b0435a
Thabang Mabula <1364103@students.wits.ac.za>
2019-07-28 22:40:42 +0200

    Removed commands from db file and made it exclusively for connecting

--- mobile_application/db.py
+++ mobile_application/db.py
@@ -17,22 +17,3 @@ load_dotenv(dotenv_path)
 conn = pymssql.connect(server=SERVER_NAME, user=USER_NAME, password=PASSWORD, database=DATABASE)
 
 cursor = conn.cursor()
-
-cursor.execute("""
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='users' and xtype='U')
-          CREATE TABLE id_photos (
-          photo_id int IDENTITY(1,1) PRIMARY KEY,
-          photo varbinary
-          )
-""")
-
-cursor.execute("""
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='users' and xtype='U')
-          CREATE TABLE feature_vectors (
-          photo_id int IDENTITY(1,1) PRIMARY KEY,
-          feature_vector varbinary
-          )
-""")
-
-conn.commit()
-conn.close()
