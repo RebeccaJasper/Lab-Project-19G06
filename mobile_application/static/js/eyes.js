f1f60b24da98cd4af27f25433379c1b187340e13
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-07-30 15:05:03 +0200

    Both eyes displayed

--- mobile_application/static/js/eyes.js
+++ mobile_application/static/js/eyes.js
@@ -2,20 +2,20 @@ import * as generateFace from './generateFace.js'
 
 // Facial markers related to Dlib are numbered
 // Left eye
-var thirtySix = new THREE.Vector2(0, 0)
-var thirtySeven = new THREE.Vector2(0, 100)
-var thirtyEight = new THREE.Vector2(0, 200)
-var thirtyNine = new THREE.Vector2(200, 200)
-var forty = new THREE.Vector2(200, 100)
-var fortyOne = new THREE.Vector2(200, 0)
+var thirtySix = new THREE.Vector2(-113, 138)
+var thirtySeven = new THREE.Vector2(-94, 150)
+var thirtyEight = new THREE.Vector2(-71, 150)
+var thirtyNine = new THREE.Vector2(-49, 135)
+var forty = new THREE.Vector2(-94, 131)
+var fortyOne = new THREE.Vector2(-71, 131)
 
-// // Right eye
-// var fortyTwo = new THREE.Vector2()
-// var fortyThree = new THREE.Vector2()
-// var fortyFour = new THREE.Vector2()
-// var fortyFive = new THREE.Vector2()
-// var fortySix = new THREE.Vector2()
-// var fortySeven = new THREE.Vector2()
+// Right eye
+var fortyTwo = new THREE.Vector2(49, 135)
+var fortyThree = new THREE.Vector2(71, 150)
+var fortyFour = new THREE.Vector2(94, 150)
+var fortyFive = new THREE.Vector2(113, 138)
+var fortySix = new THREE.Vector2(94, 131)
+var fortySeven = new THREE.Vector2(71, 131)
 
 var eyeColour = 0xffffff
 var material = new THREE.MeshLambertMaterial({ color: eyeColour })
@@ -24,16 +24,28 @@ var leftEyeCurve = new THREE.SplineCurve([
   thirtySix, thirtySeven, thirtyEight, thirtyNine, forty, fortyOne, thirtySix
 ])
 
+var rightEyeCurve = new THREE.SplineCurve([
+  fortyTwo, fortyThree, fortyFour, fortyFive, fortySix, fortySeven, fortyTwo
+])
+
 var extrudeSettings = {
-  steps: 1,
-  depth: 1,
-  bevelEnabled: false
+  steps: 50,
+  depth: 100,
+  bevelEnabled: true,
+  bevelThickness: 100,
+  bevelSize: 50,
+  bevelOffset: 50,
+  bevelSegments: 10
 }
 
 var leftEyeShape = new THREE.Shape(leftEyeCurve.getSpacedPoints(100))
-var geometry = new THREE.ExtrudeGeometry(leftEyeShape, extrudeSettings)
-var leftEye = new THREE.Mesh(geometry, material)
-
+var geometryLeft = new THREE.ExtrudeGeometry(leftEyeShape, extrudeSettings)
+var leftEye = new THREE.Mesh(geometryLeft, material)
 generateFace.scene.add(leftEye)
+
+var rightEyeShape = new THREE.Shape(rightEyeCurve.getSpacedPoints(100))
+var geometryRight = new THREE.ExtrudeGeometry(rightEyeShape, extrudeSettings)
+var rightEye = new THREE.Mesh(geometryRight, material)
+generateFace.scene.add(rightEye)
 
 // export {}
