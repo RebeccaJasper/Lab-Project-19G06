19039e76f003b88cfb92b2aefebe1cc3c7306582
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-07-31 13:36:30 +0200

    Mouth is symmetrical

--- mobile_application/static/js/mouth.js
+++ mobile_application/static/js/mouth.js
@@ -1,25 +1,30 @@
 import * as gameLogic from './gameLogic.js'
 
 // Upper lip
-var fortyEight = new THREE.Vector2(-60, -20)
-var fortyNine = new THREE.Vector2(-36, -6)
-var fifty = new THREE.Vector2(-14, 5)
+var fortyEight = new THREE.Vector2(-64, -20)
+var fortyNine = new THREE.Vector2(-38, -6)
+var fifty = new THREE.Vector2(-15, 5)
 var fiftyOne = new THREE.Vector2(0, 0)
 var fiftyTwo = new THREE.Vector2(15, 5)
-var fiftyThree = new THREE.Vector2(39, -6)
-var fiftyFour = new THREE.Vector2(68, -20)
+var fiftyThree = new THREE.Vector2(38, -6)
+var fiftyFour = new THREE.Vector2(64, -20)
+
+var sixty = new THREE.Vector2(-53, -20)
+var sixtyOne = new THREE.Vector2(-15, -14)
+var sixtyTwo = new THREE.Vector2(0, -15)
+var sixtyThree = new THREE.Vector2(15, -14)
+var sixtyFour = new THREE.Vector2(53, -20)
 
 // Lower lip
-var fiftyFive = new THREE.Vector2(40, -32)
+var fiftyFive = new THREE.Vector2(38, -32)
 var fiftySix = new THREE.Vector2(16, -38)
 var fiftySeven = new THREE.Vector2(0, -39)
-var fiftyEight = new THREE.Vector2(-15, -38)
-var fiftyNine = new THREE.Vector2(-36, -32)
-var sixty = new THREE.Vector2(-49, -20)
+var fiftyEight = new THREE.Vector2(-16, -38)
+var fiftyNine = new THREE.Vector2(-38, -32)
 
-// other
-// var sixtyOne = new THREE.Vector2()
-// var sixtyTwo = new THREE.Vector2()
+var sixtyFive = new THREE.Vector2(15, -15)
+var sixtySix = new THREE.Vector2(0, -17)
+var sixtySeven = new THREE.Vector2(-15, -15)
 
 var lipColour = 0xff6666
 var material = new THREE.MeshLambertMaterial({ color: lipColour })
@@ -34,12 +39,14 @@ var extrudeSettings = {
   bevelSegments: 10
 }
 
-var upperLipCurve = new THREE.SplineCurve([ fortyEight, fortyNine, fifty, fiftyOne, fiftyTwo, fiftyThree, fiftyFour, fortyEight ])
+var upperLipCurve = new THREE.SplineCurve([ fortyEight, fortyNine, fifty, fiftyOne, fiftyTwo, fiftyThree, fiftyFour,
+  sixtyFour, sixtyThree, sixtyTwo, sixtyOne, sixty ])
 var upperLipShape = new THREE.Shape(upperLipCurve.getSpacedPoints(50))
 var upperLipGeometry = new THREE.ExtrudeGeometry(upperLipShape, extrudeSettings)
 var upperLip = new THREE.Mesh(upperLipGeometry, material)
 
-var lowerLipCurve = new THREE.SplineCurve([ fiftyFour, fiftyFive, fiftySix, fiftySeven, fiftyEight, fiftyNine, sixty, fiftyFour ])
+var lowerLipCurve = new THREE.SplineCurve([ fortyEight, sixtySeven, sixtySix, sixtyFive, fiftyFour,
+  fiftyFive, fiftySix, fiftySeven, fiftyEight, fiftyNine ])
 var lowerLipShape = new THREE.Shape(lowerLipCurve.getSpacedPoints(50))
 var lowerLipGeometry = new THREE.ExtrudeGeometry(lowerLipShape, extrudeSettings)
 var lowerLip = new THREE.Mesh(lowerLipGeometry, material)
