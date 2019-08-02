8d38fe3cb423eed7ae01943436d02ad4f60ee31e
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-02 17:11:55 +0200

    Can toggle information based on whether a person has been seen in the past

--- mobile_application/static/js/statement.js
+++ mobile_application/static/js/statement.js
@@ -1,4 +1,18 @@
 $(document).ready(() => {
+
+    // Toggle all further details based on whether the person is known or not
+    $("#known-before-yes").on('change', () => {
+        if ($("#known-before-yes").is(':checked')) {
+            $('#details').css('display','block')
+        }
+    })
+    $("#known-before-no").click(() => {
+        if ($("#known-before-no").is(':checked')) {
+            $('#details').css('display','none')
+        }
+    })
+
+    // Toggle the input fields for person's name
     $("#person-name-yes").on('change', () => {
         $('#name-is-known').toggle()
     })
