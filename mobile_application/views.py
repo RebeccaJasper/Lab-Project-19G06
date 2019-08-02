9291cf260d126d263692085d81d43e085ea547ac
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-02 12:17:10 +0200

    Added page and route for submitting statements

--- mobile_application/views.py
+++ mobile_application/views.py
@@ -2,6 +2,12 @@ from flask import Blueprint, render_template
 
 mobile_application = Blueprint('mobile_application', __name__)
 
+
 @mobile_application.route('/')
 def index():
     return render_template('index.html')
+
+
+@mobile_application.route('/statement')
+def statement():
+    return render_template('statement.html')
