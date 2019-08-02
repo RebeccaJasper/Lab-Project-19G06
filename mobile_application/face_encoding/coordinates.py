a488c1bbfa0d4b0af4b019065bc2bec4754c8a1e
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-02 10:39:47 +0200

    Added docstring for coordinate class

--- mobile_application/face_encoding/coordinates.py
+++ mobile_application/face_encoding/coordinates.py
@@ -1,14 +1,32 @@
 from math import sqrt
 
+
 class Coordinate:
+    """Class for modeling the (x,y) coordinate of a facial feature"""
+
     def __init__(self, x: float, y: float):
+        """
+        Default constructor
+
+        :param x: x-coordinate
+        :type: float
+        :param y: y-coordinate
+        :type: float
+        """
         self.x = x
         self.y = y
 
     def __str__(self):
+        """
+        String representation of coordinate
+
+        :return: String representation of coordinate in form (x, y)
+        :rtype: str
+        """
         return '({self.x}, {self.y})'.format(self=self)
 
 def unit_vector(dlib_point):
+    
     modulus = sqrt(pow(dlib_point.x, 2) + pow(dlib_point.y, 2))
     if modulus != 0:
         x = dlib_point.x /modulus
