bcf6407c6cb8b4e0223cc61b747a9a51033b2d3b
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-02 10:19:38 +0200

    Tested the picture to base64 function

--- mobile_application/models.py
+++ mobile_application/models.py
@@ -1,4 +1,3 @@
-from typing import IO
 from mobile_application.db import *
 from typing import List
 from base64 import b64encode
@@ -9,6 +8,7 @@ def photo_to_string(filename: str) -> str:
     Opens a specified photo and converts it to a string using base64 encoding
 
     :param filename: name of the image file
+    :type: str
     :return: Base64 encoded image
     :rtype: str
     """
@@ -40,6 +40,7 @@ def convert_list_to_str(input_list: List[float]) -> str:
     Converts a list into a csv string
 
     :param input_list: Input list to be converted to a string
+    :type: List[float]
     :return: A csv string with all the elements in the input list
     :rtype: str
     """
@@ -53,7 +54,9 @@ def add_feature_vector_to_db(person_id: str, feature_vector: List[float]) -> Non
     Adds a feature vector to the database
 
     :param feature_vector: List representing a feature vector
+    :type: str
     :param person_id: String representing the id associated with the person
+    :type: str
     :rtype: None
     """
 
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,29 @@
-def func(x):
-    return x + 1
+import unittest
+from mobile_application.models import photo_to_string
+from os import path, getcwd
 
 
-def test_answer():
-    assert func(4) == 5
+class TestFacialEncoding(unittest.TestCase):
+
+    def test_base64_encoding(self):
+        """
+        Test that the function that converts the images into ASCII characters using base64 encoding returns the correct
+        string
+        """
+        # Navigate to facial_images directory
+        root_directory = path.dirname(getcwd())
+        photo_directory = path.join(root_directory, 'mobile_application\\facial_images\\1.jpg')
+        base64_file_directory = path.join(root_directory, 'mobile_application\\facial_images\\base64.txt')
+
+        # Find string representation of base64 encoding
+        test_encoding = photo_to_string(photo_directory)
+        test_encoding = open(base64_file_directory, 'r')
+
+        self.assertEqual(test_encoding, test_encoding, "Should be the same")
+        test_encoding.close()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
