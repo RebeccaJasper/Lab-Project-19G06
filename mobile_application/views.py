5d3b223e96536216e29a180468a8a654da21bf7a
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-02 19:26:13 +0200

    Added html page to display image from static reference

--- mobile_application/views.py
+++ mobile_application/views.py
@@ -5,3 +5,7 @@ mobile_application = Blueprint('mobile_application', __name__)
 @mobile_application.route('/')
 def index():
     return render_template('index.html')
+
+@mobile_application.route('/photo')
+def picture():
+    return render_template('photo.html')
