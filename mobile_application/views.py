898245ba50d982576272806bc23955893a493b1f
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-03 13:32:16 +0200

    Created grey landing page

--- mobile_application/views.py
+++ mobile_application/views.py
@@ -3,5 +3,5 @@ from flask import Blueprint, render_template
 mobile_application = Blueprint('mobile_application', __name__)
 
 @mobile_application.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
