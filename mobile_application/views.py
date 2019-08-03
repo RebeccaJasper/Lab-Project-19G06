f8cf2630ab9c9533dc72eec600f8eb1818f8c1df
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-03 18:03:53 +0200

    Routing completed from home to explanation to consent

--- mobile_application/views.py
+++ mobile_application/views.py
@@ -2,6 +2,12 @@ from flask import Blueprint, render_template
 
 mobile_application = Blueprint('mobile_application', __name__)
 
+
 @mobile_application.route('/')
 def home():
     return render_template('home.html')
+
+
+@mobile_application.route('/explanation')
+def explain():
+    return render_template('explanation.html')
