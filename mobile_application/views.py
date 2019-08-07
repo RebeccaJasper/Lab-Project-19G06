98ffd27bb2e4f4294c5763c970b2f6c99c9d4ea4
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-07 08:27:29 +0200

    Remove database calls to confirm that deployment can take place

--- mobile_application/views.py
+++ mobile_application/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, jsonify
 from os import path, getcwd
-from mobile_application.models import get_random_image
+# from mobile_application.models import get_random_image
 
 from flask import Blueprint, render_template, jsonify,request
 
@@ -38,10 +38,10 @@ def picture():
     return render_template('photo.html')
 
 
-@mobile_application.route('/api/photo', methods=['POST'])
-def picture_base64():
-    (person_id, base64_image_string) = get_random_image()
-    return jsonify(img_string=base64_image_string, person_id=person_id)
+# @mobile_application.route('/api/photo', methods=['POST'])
+# def picture_base64():
+    # (person_id, base64_image_string) = get_random_image()
+    # return jsonify(img_string=base64_image_string, person_id=person_id)
 
 
 @mobile_application.route('/statement')
