e30910e24ac9df5e008b86b58adbd9346a2cbf6d
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-08 16:11:11 +0200

    Made a function to generate new person ids

--- mobile_application/upload_images.py
+++ mobile_application/upload_images.py
@@ -1,7 +1,27 @@
 from mobile_application.models import *
 from os import listdir
 from os.path import join
+from random import randint
+
+existing_ids = []
+
+def generate_id():
+
+    UPPER_LIMIT = 2000000
+    LOWER_LIMIT = 1000000
+    run = True
+
+    new_id = randint(LOWER_LIMIT, UPPER_LIMIT)
+
+    while run:
+        if new_id in existing_ids:
+            run = True
+            new_id = randint(LOWER_LIMIT, UPPER_LIMIT)
+        else:
+            run = False
+    return new_id
+
 
 for i in listdir('./facial_images'):
     filename = join('./facial_images', i)
-    print(filename)
+    
