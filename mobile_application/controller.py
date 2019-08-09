bc0e1be57f15e7420c8acb7a1592ddd69ee66334
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-09 17:25:28 +0200

    Normalized the feature vector from the saved identikit

--- mobile_application/controller.py
+++ mobile_application/controller.py
@@ -1,9 +1,13 @@
 from mobile_application.models import save_identikit_info_to_db
+from mobile_application.face_encoding.normalization import normalize_feature_vector
 
 
 def process_submission_info(firstname: str = "", surname: str = "", gender: float = "0.25", race: float = "0.16", person_id:int = -1):
     save_identikit_info_to_db(firstname, surname, gender, race, person_id)
 
+
 def process_submission_feature_vector(feature_vector_string: str) -> None:
     feature_vector = list(map(int, feature_vector_string.split(',')))
+    feature_vector = normalize_feature_vector(feature_vector)
     print(feature_vector)
+
