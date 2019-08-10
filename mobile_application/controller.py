43600bb5e5b830701659f107d1fcc5f3457c3958
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-08-10 15:45:32 +0200

    Changed default gender to 'unknown' (1)

--- mobile_application/controller.py
+++ mobile_application/controller.py
@@ -2,7 +2,7 @@ from mobile_application.models import save_identikit_info_to_db, add_feature_vec
 from mobile_application.face_encoding.normalization import normalize_feature_vector
 
 
-def process_submission_info(firstname: str = "", surname: str = "", gender: float = "0.25", race: float = "0.16", person_id:int = -1):
+def process_submission_info(firstname: str = "", surname: str = "", gender: float = "1", race: float = "0.16", person_id:int = -1):
     save_identikit_info_to_db(firstname, surname, gender, race, person_id)
 
 
