a4941a35a5ea0b5a18771d48648cadeaf339bc58
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-11 14:00:43 +0200

    Added weights fro various features

--- mobile_application/feature_weighting.py
+++ mobile_application/feature_weighting.py
@@ -1,5 +1,16 @@
 EYES = list(range(8, 20))
 NOSE = list(range(6, 8))
 MOUTH = list(range(20, 24))
-print(MOUTH)
 
+face_feature_weights = {
+    "Eyes": 0.81,
+    "Nose": 0.088,
+    "Mouth": 0.022,
+    "Other": 0.08,
+}
+
+feature_weights = {
+    "Race": 0.4,
+    "Gender": 0.4,
+    "Face": 0.2
+}
