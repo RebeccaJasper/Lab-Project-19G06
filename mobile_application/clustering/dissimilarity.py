e0d35de2b9829cd4ddbc54f6d648d0de0497f56f
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-14 15:54:35 +0200

    Added function calculate feature ranges

--- mobile_application/clustering/dissimilarity.py
+++ mobile_application/clustering/dissimilarity.py
@@ -3,6 +3,11 @@ from feature_encoding import *
 from feature_weighting import *
 from mobile_application.face_encoding.coordinates import Coordinate
 from sklearn import preprocessing
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import squareform
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+import scipy.cluster.hierarchy as shc
 
 
 class UnequalArrayLength(Exception):
@@ -18,9 +23,10 @@ class NonBinaryFormat(Exception):
 class Dissimilarity(object):
     __LENGTH_OF_VECTOR = 4
     __distance_vector = np.array([])
+    __feature_vectors = np.array([])
 
     def __init__(self):
-        self._feature_vectors = np.array([])
+        self.__feature_vectors = np.array([])
 
     @property
     def feature_vectors(self):
@@ -32,23 +38,33 @@ class Dissimilarity(object):
     def add_vector(self, vector: np.array) -> None:
         self.__feature_vectors = np.vstack((self.__vectors, vector))
 
+
+    def ranges(self):
+        """Function for calculating in the range on each column in the feature_matrix"""
+
+        ranges = np.ptp(self.__feature_vectors, axis=0)
+        return ranges
+
     def distance_matrix(self)-> np.array:
-        np.delete(feature_matrix, np.arange(0, feature_matrix.shape[0]), 0)
+        #
+        # np.delete(feature_matrix, np.arange(0, feature_matrix.shape[0]), 0)
+
         distance_matrix = np.array([])
         for currrent_row_index in np.arange(0, self.__feature_vectors.shape[0]):
-            for other_row_index in np.arange(0, self.__feature_vectors.shape[0]):
-                distance_vector = Dissimilarity.distance(self.__feature_vectors[currrent_row_index],
-                                                         self.__feature_vectors[other_row_index])
-                if currrent_row_index == 0 and other_row_index == 0:
-                    distance_matrix = np.hstack((distance_matrix, distance_vector))
-                else:
-                    distance_matrix = np.vstack((distance_matrix, distance_vector))
+            start = currrent_row_index + 1
+            # for other_row_index in np.arange(start, self.__feature_vectors.shape[0]):
+            if start < self.__feature_vectors.shape[0]:
+                for other_row_index in np.arange(start, self.__feature_vectors.shape[0]):
+                    distance = Dissimilarity.distance(self.__feature_vectors[currrent_row_index],
+                                                             self.__feature_vectors[other_row_index])
+
+                    distance_matrix = np.append(distance_matrix, [distance])
 
         return distance_matrix
 
 
     @staticmethod
-    def distance(vector_1: np.array, vector_2: np.array)-> np.array:
+    def distance(vector_1: np.array, vector_2: np.array, range: np.array, weights: np.array)-> np.array:
         """
         Calculate dissimilary between feature vectors
 
@@ -58,30 +74,40 @@ class Dissimilarity(object):
 
         # Used a Python List instead of numpy array since Python Lists are mutable.
         # This allows for new dissimilariy values to be appended to the vector
-        dissimilarity_vector = []
+        dist = 0
 
         # max_abs_scaler = preprocessing.MaxAbsScaler()
 
-        for x, y in zip(*[iter(feature_vector_indexes["Face"])] * 2):
-            face_coordinate_1 = np.array([vector_1[x], vector_1[y]])
-            face_coordinate_2 = np.array([vector_2[x], vector_2[y]])
-            dissimilarity = Dissimilarity.gower_similarity(face_coordinate_1, face_coordinate_2, datatype="ratio")
-            dissimilarity_vector.append(dissimilarity)
+        for i in feature_vector_indexes["Face"]:
+            partial_dist = Dissimilarity.gower_similarity(np.array(vector_1[i]), np.array(vector_2[i]))
+            partial_dist = partial_dist/range[i]
+            partial_dist = partial_dist * weights[i]
+            partial_dist = partial_dist/weights.sum()
+
+            dist += partial_dist
+
+        # for x, y in zip(*[iter(feature_vector_indexes["Face"])] * 2):
+        #     face_coordinate_1 = np.array([vector_1[x], vector_1[y]])
+        #     face_coordinate_2 = np.array([vector_2[x], vector_2[y]])
+        #     partial_dissimilarity = Dissimilarity.gower_similarity(face_coordinate_1, face_coordinate_2, datatype="ratio")
+        #
+        #     dissimilarity_vector.append(dissimilarity)
 
         # dissimilarity_vector = max_abs_scaler.fit_transform(dissimilarity_vector)
 
         race_vector_1 = vector_1[feature_vector_indexes["Race"]]
         race_vector_2 = vector_2[feature_vector_indexes["Race"]]
         race_dissimilarity = Dissimilarity.gower_similarity(race_vector_1, race_vector_2, datatype="nominal")
-        dissimilarity_vector.append(race_dissimilarity)
+        dist += race_dissimilarity
 
         gender_vector_1 = vector_1[feature_vector_indexes["Gender"]]
         gender_vector_2 = vector_2[feature_vector_indexes["Gender"]]
         gender_dissimilarity = Dissimilarity.gower_similarity(gender_vector_1, gender_vector_2, datatype="nominal")
-        dissimilarity_vector.append(gender_dissimilarity)
+        dist += race_dissimilarity
 
-        return np.array(dissimilarity_vector)
+        dist = dist/vector_1.size
 
+        return dist
 
     @staticmethod
     def gower_similarity(vector_1: np.array, vector_2: np.array, datatype: str ="ratio")-> float:
@@ -139,11 +165,27 @@ class Dissimilarity(object):
 
         return coef
 
-#
-# feature_matrix = np.array(([1, 2, 4, 5, 1, 0, 0, 0, 1, 0], [1, 3, 6, 5, 0, 1, 0, 0, 1, 0]))
-#
-# # print('After deletion:')
-# # print(np.delete(feature_matrix, np.arange(0, feature_matrix.shape[0]), 0))
+
+feature_matrix = np.array(([1, 2, 4, 5, 1, 0, 0, 0, 1, 0], [1, 3, 6, 5, 0, 1, 0, 0, 1, 0], [1, 3, 6, 5, 0, 1, 0, 0, 0, 1]))
+d = Dissimilarity()
+d.load_feature_vectors(feature_matrix)
+print(d.ranges())
+
+# print('After deletion:')
+# print(np.delete(feature_matrix, np.arange(0, feature_matrix.shape[0]), 0))
 # d = Dissimilarity()
 # d.load_feature_vectors(feature_matrix)
-# print(d.distance_matrix())
+# matrix = d.distance_matrix()
+#
+#
+#
+# plt.figure(figsize=(10, 7))
+# plt.title("Dendrograms")
+# dists = squareform(matrix)
+# linkage_matrix = linkage(dists, "single")
+# # dend = shc.dendrogram(shc.linkage(dists, "single"))
+# dendrogram(linkage_matrix)
+# plt.axhline(y=6, color='r', linestyle='--')
+# plt.show()
+
+
--- mobile_application/clustering/hierarchical_clusterring.py
+++ mobile_application/clustering/hierarchical_clusterring.py
@@ -1,6 +1,7 @@
+import numpy as np
 
 class HeirachicalClustering(object):
     def __init__(self):
         return
 
-    
+    def cluster(self, distance_matrix: np.array) -> None:
