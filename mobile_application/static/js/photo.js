83daf63ca8c8657b84149c6efeea53fca26b705a
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-16 22:46:13 +0200

    Added message if rooting happens during viewing

--- mobile_application/static/js/photo.js
+++ mobile_application/static/js/photo.js
@@ -8,11 +8,25 @@ $(document).ready(() => {
             $('#image-area').append("<img src='data:image/jpg;base64," + res.img_string + "'>")
             sessionStorage.setItem("photo_id", res.person_id)
 
-            let VIEWING_DURATION = 5000
+            let VIEWING_DURATION = 9000
 
             setTimeout(() => {
                 window.location = "/statement"
             }, VIEWING_DURATION)
         }
     })
+
 })
+
+//$(window).on('beforeunload', () => {
+////  console.log('Tried to unload')
+////  window.alert("Please stay on the site and view the image for the full 9 seconds")
+////  window.location = "/photo"
+//  return "Please stay on the site and view the image for the full 9 seconds";
+//});
+
+window.addEventListener("beforeunload", function(e) {
+     e.preventDefault();
+     e.returnValue = false;
+     return "Please stay on the site and view the image for the full 9 seconds";
+});
