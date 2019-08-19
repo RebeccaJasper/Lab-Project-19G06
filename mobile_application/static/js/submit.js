f406feee72f76aea202e09f24fbdcd2107295c37
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-08-19 14:55:40 +0200

    Loader appears on submit

--- mobile_application/static/js/submit.js
+++ mobile_application/static/js/submit.js
@@ -1,10 +1,22 @@
 import * as markers from './GUI.js'
 import * as gameWindow from './gameLogic.js'
 
+function showLoader () {
+  $('.loader').fadeIn('slow')
+}
+
+function hideLoader () {
+  $('.loader').fadeOut('slow')
+}
+
+hideLoader()
 $(document).ready(() => {
-    window.sessionStorage.setItem("prevRoute", window.location)
+  window.sessionStorage.setItem('prevRoute', window.location)
+
   $('.submit-btn').click(() => {
     $('.submit-btn').attr('disabled', true)
+    showLoader()
+
     let statement = JSON.parse(window.sessionStorage.getItem('statement'))
     let person_id = JSON.parse(window.sessionStorage.getItem('photo_id'))
 
@@ -25,19 +37,17 @@ $(document).ready(() => {
       'eyeColour': colours.eyeColour
     }
 
-    
-        $.ajax({
-          url: '/api/submit',
-          method: 'POST',
-          contentType: 'application/json',
-          data: JSON.stringify(submission),
-          success: function (res) {
-            console.log('statement and identikit submitted')
-            console.log(submission)
-            sessionStorage.clear()
-            window.location = '/complete'
-          }
-        })
+    $.ajax({
+      url: '/api/submit',
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(submission),
+      success: function (res) {
+        console.log('statement and identikit submitted')
+        console.log(submission)
+        window.sessionStorage.clear()
+        window.location = '/complete'
+      }
     })
-          
+  })
 })
