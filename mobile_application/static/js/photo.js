223f8683005b1231e779a39c46fd47f358d8236d
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-08-19 15:14:32 +0200

    Loader added to photo page

--- mobile_application/static/js/photo.js
+++ mobile_application/static/js/photo.js
@@ -1,42 +1,29 @@
 var allowRouting = false
 
+$('.loader').fadeIn('slow')
+
 $(document).ready(() => {
-    if (typeof window.sessionStorage.getItem('photo_id') == 'undefined' ||
-        typeof window.sessionStorage.getItem('photo_id') == 'object') {
-         $.ajax({
-            url: '/api/photo',
-            method: 'POST',
-            contentType: 'application/json',
-            success: function (res) {
-                $('#image-area').append("<img src='data:image/jpg;base64," + res.img_string + "'>")
-                sessionStorage.setItem("photo_id", res.person_id)
+  if (typeof window.sessionStorage.getItem('photo_id') === 'undefined' ||
+        typeof window.sessionStorage.getItem('photo_id') === 'object') {
+    $.ajax({
+      url: '/api/photo',
+      method: 'POST',
+      contentType: 'application/json',
+      success: function (res) {
+        $('.loader').fadeOut('slow')
 
-      let VIEWING_DURATION = 15000
+        $('#image-area').append("<img src='data:image/jpg;base64," + res.img_string + "'>")
+        sessionStorage.setItem('photo_id', res.person_id)
 
-                setTimeout(() => {
-                    allow_routing = true
-                    window.location = "/statement"
-                }, VIEWING_DURATION)
-            }
-        })
-    } else {
-        window.location = window.sessionStorage.getItem('prevRoute')
-    }
+        let VIEWING_DURATION = 15000
+
+        setTimeout(() => {
+          allow_routing = true
+          window.location = '/statement'
+        }, VIEWING_DURATION)
+      }
+    })
+  } else {
+    window.location = window.sessionStorage.getItem('prevRoute')
+  }
 })
-
-//$(window).on('beforeunload', () => {
-////  console.log('Tried to unload')
-////  window.alert("Please stay on the site and view the image for the full 9 seconds")
-////  window.location = "/photo"
-//  return "Please stay on the site and view the image for the full 9 seconds";
-//});
-//
-//window.addEventListener("beforeunload", function(e) {
-//    console.log(allowRouting)
-//    if (allowRouting == false) {
-//        console.log(allow_routing)
-//        e.preventDefault();
-//        e.returnValue = false;
-//        return "Please stay on the site and view the image for the full 9 seconds";
-//    }
-//});
