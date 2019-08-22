19790008db20b9fa8eaa8e1fb77394e498500658
Thabang Mabula <1364103@students.wits.ac.za>
2019-08-22 22:17:41 +0200

    Added docstrings to normalization file

--- mobile_application/face_encoding/normalization.py
+++ mobile_application/face_encoding/normalization.py
@@ -4,6 +4,14 @@ import numpy as np
 
 
 def normalize_feature_vector(feature_vector: List[int]) -> List[float]:
+    """
+    Function used to normalize facial coordinates (i.e. convert them to values between 0 to 1)
+
+    :param feature_vector: Array of facial feature points
+    :type: List[int]
+    :return: Normalized array of facial feature points
+    :rtype: List[float]
+    """
     normalized_feature_vector = []
     reference_point = Coordinate(feature_vector[42], feature_vector[43])
 
@@ -16,7 +24,16 @@ def normalize_feature_vector(feature_vector: List[int]) -> List[float]:
 
     return normalized_feature_vector
 
-def change_coordinate_reference_of__identikit_array(feature_vector: np.array) -> np.array:
+def change_coordinate_reference_of__identikit_array(feature_vector: np.array, ref_coordinate: Coordinate) -> np.array:
+    """
+    Function used to change the coordinate references of an array of feature points
+
+    :param feature_vector: Array of facial feature points.
+    :type: np.array
+    :param ref_coordinate: Reference coordinate of the new coordinate base
+    :type: Coordinate
+    :return: Array of feature points
+    """
     changed_feature_vector = []
     reference_point = Coordinate(feature_vector[100], feature_vector[101])
     for x, y in zip(*[iter(feature_vector)]*2):
