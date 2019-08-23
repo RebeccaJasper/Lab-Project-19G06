3cbc6078f5fa66d243ee912c09a4e9c1d5a5709c
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-08-23 15:47:00 +0200

    Landing page styling

---
+++ mobile_application/static/js/landing-page.js
@@ -0,0 +1,15 @@
+if ($(window).width() < 400) {
+  $('.brand-name').css('font-size', '5px')
+}
+
+// $('.loader').fadeIn('slow')
+
+$(document).ready(() => {
+//   $('.loader').fadeOut('slow')
+
+  //   $('#home-page-btn').click(() => {
+
+  //   })
+
+  // window.location = window.sessionStorage.getItem('prevRoute')
+})
