2635194a5f0f14e5979f3d1e67a91c5f975febf9
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-08-23 15:02:02 +0200

    Added function to calculate optimal weights (in progress)

--- mobile_application/clustering/CPCC.py
+++ mobile_application/clustering/CPCC.py
@@ -1,12 +1,16 @@
 from dissimilarity import Dissimilarity
 from scipy.cluster.hierarchy import cophenet
+from scipy.optimize import minimize
 import numpy as np
 
 
-def calculate_CPCC(matrix: np.array)-> float:
-    distance_matrix = Dissimilarity.distance_matrix(matrix)
+def calculate_CPCC()-> float:
+    distance_matrix = Dissimilarity.distance_matrix()
     cophenetic_matrix = cophenet(distance_matrix)
     CPCC = np.corrcoef(distance_matrix, cophenetic_matrix)[0,1]
 
     return CPCC
 
+def calculate_optimal_weights() -> np.array
+    x0 = np.array([]) # initial guess(es)
+    return minimize(calculate_CPCC(), x0, method=‘BFGS’ )
