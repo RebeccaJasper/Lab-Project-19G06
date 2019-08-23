0aa805aed68ed7602e7eb60e3f308e3b23292c40
Rebecca Jasper <rebecca.courtjasper@gmail.com>
2019-08-23 15:54:40 +0200

    Buttons route to appropriate URLs

--- mobile_application/static/js/landing-page.js
+++ mobile_application/static/js/landing-page.js
@@ -2,14 +2,18 @@ if ($(window).width() < 400) {
   $('.brand-name').css('font-size', '5px')
 }
 
-// $('.loader').fadeIn('slow')
+$('.loader').fadeIn('slow')
 
 $(document).ready(() => {
-//   $('.loader').fadeOut('slow')
-
-  //   $('#home-page-btn').click(() => {
-
-  //   })
+  $('.loader').fadeOut('slow')
 
   // window.location = window.sessionStorage.getItem('prevRoute')
 })
+
+let identiKreate = function () {
+  window.location = '/explanation'
+}
+
+let identiMatch = function () {
+  window.location = '/submissions'
+}
